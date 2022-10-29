9520d3df0ff4c9d8f2e1413420136b9ccee6894c
PiSITE <116979505+PiSITE@users.noreply.github.com>
2022-10-30 01:06:51 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -91,20 +91,8 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
         <div className="flex flex-row justify-between mt-4 text-sm">
           <div>
             Powered by{' '}
-            <a href="https://workers.cloudflare.com/" target="_blank">
-              Cloudflare Workers{' '}
-            </a>
-            &{' '}
-            <a href="https://flareact.com/" target="_blank">
-              Flareact{' '}
-            </a>
-          </div>
-          <div>
-            <a
-              href="https://github.com/eidam/cf-workers-status-page"
-              target="_blank"
-            >
-              Get Your Status Page
+            <a href="https://discord.gg/piratesite" target="_blank">
+              PSITE{' '}
             </a>
           </div>
         </div>
